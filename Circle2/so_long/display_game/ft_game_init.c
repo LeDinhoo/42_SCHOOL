e14bc5a405f175a2f89797a077729776528b69cc
Dinho <darksheep42350@gmail.com>
2023-01-12 18:25:03 +0100

    Makefile & MLX Linux

--- Circle2/so_long/display_game/ft_game_init.c
+++ Circle2/so_long/display_game/ft_game_init.c
@@ -6,19 +6,43 @@
 /*   By: hdupuy <hdupuy@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:22:56 by hdupuy            #+#    #+#             */
-/*   Updated: 2023/01/12 14:29:28 by hdupuy           ###   ########.fr       */
+/*   Updated: 2023/01/12 18:05:27 by hdupuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
 int	ft_display_init(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
+	t_data	img2;
+
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Bonjour");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 80, 80);
 	mlx_loop(mlx);
-	return (0);
 }
