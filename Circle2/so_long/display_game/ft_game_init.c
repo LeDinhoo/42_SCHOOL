e15402de6c368698f560296eb68b8a489b36a0ff
Dinho <darksheep42350@gmail.com>
2023-01-19 10:46:56 +0100

    All good

--- Circle2/so_long/display_game/ft_game_init.c
+++ Circle2/so_long/display_game/ft_game_init.c
@@ -6,17 +6,12 @@
 /*   By: hdupuy <hdupuy@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:22:56 by hdupuy            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/01/17 17:12:40 by hdupuy           ###   ########.fr       */
-=======
-/*   Updated: 2023/01/12 18:05:27 by hdupuy           ###   ########.fr       */
->>>>>>> e14bc5a405f175a2f89797a077729776528b69cc
+/*   Updated: 2023/01/19 10:46:38 by hdupuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
-<<<<<<< HEAD
 t_image	ft_new_sprite(void *mlx, char *path)
 {
 	t_image	img;
@@ -60,39 +55,4 @@ int	ft_display_init(t_map *map)
 	}
 	mlx_loop(program.mlx);
 	return (0);
-=======
-typedef struct s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
-
-int	ft_display_init(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-	t_data	img2;
-
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Bonjour");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-			&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 80, 80);
-	mlx_loop(mlx);
->>>>>>> e14bc5a405f175a2f89797a077729776528b69cc
 }
